99d942835053d01c82acedb82ddd656d88bc011c
rostnrol <112760457+rostnrol@users.noreply.github.com>
2023-11-20 20:43:44 +0400

    Update Program.cs

--- BrothersSafeGame1/Program.cs
+++ BrothersSafeGame1/Program.cs
@@ -8,9 +8,6 @@ namespace BrothersSafeGame1
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
